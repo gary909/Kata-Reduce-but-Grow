6486714ab654fd23c8ac1e50970f99ba68a9d2c9
gary <gary.white@lodgit.com>
2021-10-28 12:54:10 +0200

    code & html

---
+++ kataReduce.js
@@ -0,0 +1,11 @@
+function grow(x){
+    var sum = 0;
+    for (var i = 0; i < x.length; i++){
+
+    }
+    return x.length;
+}
+
+console.log(grow([1, 2, 3])); // 6
+console.log(grow([4, 1, 1, 1, 4])); // 16
+console.log(grow([2, 2, 2, 2, 2, 2])); // 64
